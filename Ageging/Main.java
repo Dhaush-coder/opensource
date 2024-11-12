dfa1fd803ac9bf469bd4998586dd02e0a26cdadc
p.dhanush <238r1a0542@cmrec.ac.in>
2024-11-13 00:46:46 +0530

    Create Main.java

---
+++ Ageging/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int x = sc.nextInt();
+            int y = x-10;
+            System.out.println(y);
+        }
+    }
+}
