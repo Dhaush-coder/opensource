01f6a7fc34173ab8efa62763b95615b8bc62e26d
p.dhanush <238r1a0542@cmrec.ac.in>
2024-11-14 22:11:34 +0530

    Create Main.java

---
+++ donation/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int i=0;i<T;i++)
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=x-y;
+            System.out.println(z);
+        }
+    }
+}
