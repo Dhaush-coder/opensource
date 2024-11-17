ca7470a89f07c54b27830c41738cd6bd22958f06
p.dhanush <238r1a0542@cmrec.ac.in>
2024-11-17 10:36:34 +0530

    Create Main.java

---
+++ budget/Main.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int t=0;t<T;t++)
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            if(x+y <= z)
+            {
+                System.out.println(2);
+            }
+            else if(x<=z)
+            {
+                System.out.println(1);
+            }
+            else
+            {
+                System.out.println(0);
+            }
+        }
+    }
+}
