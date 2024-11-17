0d1584c579bbf9a98686ec89625939e4e68a3267
p.dhanush <238r1a0542@cmrec.ac.in>
2024-11-17 11:06:03 +0530

    Create Main.java

---
+++ video/badhri/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int i=0;i<T;i++)
+        {
+            int x=sc.nextInt();
+            int totalDistance=2*x*5;
+            System.out.println(totalDistance);
+        }
+    }
+}
